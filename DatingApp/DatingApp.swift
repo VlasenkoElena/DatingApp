56e08d6a795acc2d94a3f8e719d75124dff94e01
VlasenkoHelena <33989161+VlasenkoElena@users.noreply.github.com>
2024-06-11 18:12:25 +0300

    init

---
+++ DatingApp/DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppApp.swift
+//  DatingApp
+//
+//  Created by Helen on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
